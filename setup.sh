9f1cfc12d6c5ec29048df242360cf1ae7ff6eca9
Jacob Fugal <jacob@instructure.com>
2012-08-10 13:24:56 -0600

    setup tweaks
    do the sudo in the script, rather than needing to run script under sudo
    
    have certain commands run as the new straitjacket user rather than root
    
    use install.py to setup /var/local/straitjacket

--- setup.sh
+++ setup.sh
@@ -1,38 +1,47 @@
-# tested on a VirtualBox i386 Ubuntu Precise VM. run as root
-cd .. # out of straitjacket checkout
+# tested on a VirtualBox i386 Ubuntu Precise VM
 
 # install dependencies
-aptitude install apache2-mpm-itk gcc mono-gmcs g++ guile-1.8 ghc lua5.1 ocaml \
-                 php5 python ruby ruby1.9.3 scala racket golang openjdk-6-jdk \
-                 python-pip gcc-multilib xdg-utils nodejs npm make \
-                 python-webpy python-libapparmor apparmor libapache2-mod-wsgi
+sudo aptitude install apache2-mpm-itk gcc mono-gmcs g++ guile-1.8 ghc lua5.1 \
+                      ocaml php5 python ruby ruby1.9.3 scala racket golang \
+                      openjdk-6-jdk python-pip gcc-multilib xdg-utils nodejs \
+                      npm make python-webpy python-libapparmor apparmor \
+                      libapache2-mod-wsgi
+
+# install DMD (not available in apt)
 wget http://ftp.digitalmars.com/dmd_2.060-0_i386.deb
-dpkg -i dmd_2.060-0_i386.deb
+sudo dpkg -i dmd_2.060-0_i386.deb
 rm dmd_2.060-0_i386.deb
 
-# setup and relocate
-adduser straitjacket
-mkdir -p /var/webapps /var/local/straitjacket/lib /var/local/straitjacket/tmp
-chown -R straitjacket:straitjacket /var/local/straitjacket straitjacket
-mv straitjacket /var/webapps/straitjacket
-cd /var/webapps/straitjacket/src && make
+# relocate under straitjacket user
+sudo adduser straitjacket
+sudo mkdir -p /var/webapps
+sudo mv ../straitjacket /var/webapps/straitjacket
+sudo chown -R straitjacket:straitjacket /var/webapps/straitjacket
 cd /var/webapps/straitjacket
 
-# put configurations in place
-mv src/getpwuid_r_hijack.so /var/local/straitjacket/lib/
-cp -r files/etc/apparmor.d/* /etc/apparmor.d/
-/etc/init.d/apparmor reload
-cp files/etc/apache2/sites-available/straitjacket \
-   /etc/apache2/sites-available/straitjacket
-ln -s /etc/apache2/sites-available/straitjacket /etc/apache2/sites-enabled
-rm /etc/apache2/sites-enabled/000-default
-mkdir -p static
-/etc/init.d/apache2 restart
+# setup
+sudo -u straitjacket python install.py
+sudo -u straitjacket mkdir -p /var/local/straitjacket/lib static
+cd src && sudo -u straitjacket make
+
+# put C# getpwuid hack in place
+sudo -u straitjacket mv src/getpwuid_r_hijack.so /var/local/straitjacket/lib/
+
+# configure apparmor
+sudo cp -r files/etc/apparmor.d/* /etc/apparmor.d/
+sudo /etc/init.d/apparmor reload
+
+# configure apache
+sudo cp files/etc/apache2/sites-available/straitjacket \
+        /etc/apache2/sites-available/straitjacket
+sudo ln -s /etc/apache2/sites-available/straitjacket /etc/apache2/sites-enabled
+sudo rm /etc/apache2/sites-enabled/000-default
+sudo /etc/init.d/apache2 restart
 
 # test the setup
-git checkout testing
-python server_tests.py
-python remote_server_tests.py http://localhost/
+sudo -u straitjacket git checkout testing
+sudo -u straitjacket python server_tests.py
+sudo -u straitjacket python remote_server_tests.py http://localhost/
 
 # done
-git checkout master
+sudo -u straitjacket git checkout master
