47b32af956254a13470866bd21ac1af766b0bda4
Jacob Fugal <jacob@instructure.com>
2012-08-10 10:00:16 -0600

    update setup

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ cd .. # out of straitjacket checkout
 
 # install dependencies
 aptitude install apache2-mpm-itk gcc mono-gmcs g++ guile-1.8 ghc lua5.1 ocaml \
-                 php5 python ruby ruby1.9 scala racket golang openjdk-6-jdk \
-                 python-pip gcc-multilib xdg-utils nodejs npm
+                 php5 python ruby ruby1.9.3 scala racket golang openjdk-6-jdk \
+                 python-pip gcc-multilib xdg-utils nodejs npm make
 wget http://ftp.digitalmars.com/dmd_2.060-0_i386.deb
 dpkg -i dmd_2.060-0_i386.deb
 rm dmd_2.060-0_i386.deb
@@ -14,22 +14,20 @@ adduser straitjacket
 mkdir -p /var/webapps /var/local/straitjacket/{lib,tmp}
 mv straitjacket /var/webapps/straitjacket
 cd /var/webapps/straitjacket/src && make
+cd /var/webapps/straitjacket
 
 # put configurations in place
-mv /var/webapp/straitjacket/src/getpwuid_r_hijack.so \
-   /var/local/straitjacket/lib
-cp -r /var/webapp/straitjacket/files/etc/apparmor.d/* /etc/apparmor.d/
-cp /var/webapp/straitjacket/files/etc/apache2/sites-available/straitjacket \
+mv src/getpwuid_r_hijack.so /var/local/straitjacket/lib
+cp -r files/etc/apparmor.d/* /etc/apparmor.d/
+cp files/etc/apache2/sites-available/straitjacket \
    /etc/apache2/sites-available/straitjacket
-sudo chown -R straitjacket:straitjacket /var/local/straitjacket \
-                                        /var/webapps/straitjacket
+chown -R straitjacket:straitjacket /var/local/straitjacket .
 /etc/init.d/apache2 restart
 
 # test the setup
-cd /var/webapps/straitjacket && git checkout testing
-rm /var/webapps/straitjacket/config/cache*
-python /var/webapp/straitjacket/server_test.py
-python /var/webapps/straitjacket/remote_server_test.py
+git checkout testing
+python /var/webapps/straitjacket/server_tests.py
+python /var/webapps/straitjacket/remote_server_tests.py
 
 # done
-cd /var/webapps/straitjacket && git checkout master
+git checkout master
