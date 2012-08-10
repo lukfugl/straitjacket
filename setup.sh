06bec79edfb0c6ebde85206389bc8a715474870a
Jacob Fugal <jacob@instructure.com>
2012-08-10 11:12:09 -0600

    add webpy and other non-language dependencies

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@ cd .. # out of straitjacket checkout
 # install dependencies
 aptitude install apache2-mpm-itk gcc mono-gmcs g++ guile-1.8 ghc lua5.1 ocaml \
                  php5 python ruby ruby1.9.3 scala racket golang openjdk-6-jdk \
-                 python-pip gcc-multilib xdg-utils nodejs npm make
+                 python-pip gcc-multilib xdg-utils nodejs npm make \
+                 python-webpy python-libapparmor apparmor
 wget http://ftp.digitalmars.com/dmd_2.060-0_i386.deb
 dpkg -i dmd_2.060-0_i386.deb
 rm dmd_2.060-0_i386.deb
